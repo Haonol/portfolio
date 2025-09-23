50b32162b7c821f87ff7d6ce8b25858b477741e5
Haonol <107718503+Haonol@users.noreply.github.com>
2025-09-23 15:46:45 +0900

    Update get-data.js

--- netlify/functions/get-data.js
+++ netlify/functions/get-data.js
@@ -3,9 +3,7 @@ const { getStore } = require('@netlify/blobs');
 exports.handler = async () => {
   try {
     const store = getStore('portfolioDataStore');
-    // 'main' 키로 저장된 데이터를 JSON 형태로 가져옵니다.
     const data = await store.get('main', { type: 'json' });
-
     return {
       statusCode: 200,
       body: JSON.stringify(data),

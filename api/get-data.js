2cf2fc2c0fdd4edab54daf56cc521036ee94029f
Haonol <107718503+Haonol@users.noreply.github.com>
2025-09-23 16:18:38 +0900

    Update get-data.js

--- api/get-data.js
+++ api/get-data.js
@@ -1,10 +1,11 @@
-const { kv } = require('@vercel/kv');
+const { getStore } = require('@vercel/kv');
 
 module.exports = async (req, res) => {
   try {
     const data = await kv.get('portfolioData');
     res.status(200).json(data);
   } catch (error) {
+    console.error("Get-data function error:", error);
     res.status(500).json({ error: '데이터를 불러오는 데 실패했습니다.' });
   }
 };

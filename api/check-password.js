09952705fbe9c905ec3b43f0b9ef2a0f5e713f37
Haonol <107718503+Haonol@users.noreply.github.com>
2025-09-23 16:19:04 +0900

    Update check-password.js

--- api/check-password.js
+++ api/check-password.js
@@ -17,6 +17,7 @@ module.exports = (req, res) => {
       res.status(401).json({ success: false, message: '비밀번호가 틀렸습니다.' });
     }
   } catch (error) {
+    console.error("Check-password function error:", error);
     res.status(500).json({ success: false, message: error.message });
   }
 };
